5a0793f31ae7ff6637a3cc34129e9264c96d4177
tommasogomiero <tommaso.gomiero@studenti.unipd.it>
2024-06-07 15:18:38 +0200

    Update Blocco.java
    cambio immagine blocco

--- giocofinale/src/main/java/topi/strutture/Blocco.java
+++ giocofinale/src/main/java/topi/strutture/Blocco.java
@@ -2,7 +2,7 @@ package topi.strutture;
 
 public class Blocco extends PannelloBase{
 	/* Costanti */
-	private final static String IMAGE_BLOCCO = "immagini/blocco.png";
+	private final static String IMAGE_BLOCCO = "immagini/blocco.jpg";
 
 	public Blocco(int x, int y) {
 		super(x,y);
